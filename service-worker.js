ab07971300dbcab1227d7c75a267f52b31b17491
thebehtsmusic <djthormix@gmail.com>
2025-08-07 18:14:40 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'radio-caliente-cache-v1';
+const URLS_TO_CACHE = [
+  '/',
+  '/radio_caliente_PROYECTO_BASE64.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
